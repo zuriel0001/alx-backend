3bde987d1410785e40cfc728c80b2a4ef84250b4
zuriel0001 <dogbeykwame.kd@gmail.com>
2024-01-31 20:47:10 +0000

    task 3

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""Module forx caching Least Recently used.
+"""
+from collections import OrderedDict
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    Represents an object used for storing and
+    retrieving items from a dictionary using LRU
+    removal mechanism when the limit is reached
+    """
+
+    def __init__(self):
+        """
+            method to initializes the cache
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        Method that adds an item in the cache
+        """
+        if key is None or item is None:
+            return
+
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lru_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", lru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Returns items by key
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+
+        return (self.cache_data.get(key, None))
