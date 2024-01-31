97116bbb2987ffb84c40f43a8c33e0e1df21fadc
zuriel0001 <dogbeykwame.kd@gmail.com>
2024-01-31 20:14:01 +0000

    updated

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Least Frequently Used caching module.
+"""
+    Module for caching Least Frequently Used.
 """
 from collections import OrderedDict
 
@@ -7,9 +8,8 @@ from base_caching import BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """Represents an object that allows storing and
-    retrieving items from a dictionary with a LFU
-    removal mechanism when the limit is reached.
+    """
+    inherits from BaseCaching and is a caching system
     """
     def __init__(self):
         """Initializes the cache.
@@ -19,8 +19,9 @@ class LFUCache(BaseCaching):
         self.keys_freq = []
 
     def __reorder_items(self, mru_key):
-        """Reorders the items in this cache based on the most
-        recently used item.
+        """
+        Method to reorders the items in this cache based
+        on the most recently used item.
         """
         max_positions = []
         mru_freq = 0
@@ -31,43 +32,52 @@ class LFUCache(BaseCaching):
                 mru_freq = key_freq[1] + 1
                 mru_pos = i
                 break
+
             elif len(max_positions) == 0:
                 max_positions.append(i)
             elif key_freq[1] < self.keys_freq[max_positions[-1]][1]:
                 max_positions.append(i)
         max_positions.reverse()
+
         for pos in max_positions:
             if self.keys_freq[pos][1] > mru_freq:
                 break
+
             ins_pos = pos
         self.keys_freq.pop(mru_pos)
         self.keys_freq.insert(ins_pos, [mru_key, mru_freq])
 
     def put(self, key, item):
-        """Adds an item in the cache.
+        """
+        Method that adds an item in the cache.
         """
         if key is None or item is None:
             return
+
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
                 lfu_key, _ = self.keys_freq[-1]
                 self.cache_data.pop(lfu_key)
                 self.keys_freq.pop()
                 print("DISCARD:", lfu_key)
+
             self.cache_data[key] = item
             ins_index = len(self.keys_freq)
             for i, key_freq in enumerate(self.keys_freq):
                 if key_freq[1] == 0:
                     ins_index = i
                     break
+
             self.keys_freq.insert(ins_index, [key, 0])
         else:
             self.cache_data[key] = item
             self.__reorder_items(key)
 
     def get(self, key):
-        """Retrieves an item by key.
+        """
+        Method to retrieve an item by key.
         """
         if key is not None and key in self.cache_data:
             self.__reorder_items(key)
-        return self.cache_data.get(key, None)
+
+        return (self.cache_data.get(key, None))
