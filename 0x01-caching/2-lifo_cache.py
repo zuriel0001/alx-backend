250facb15b536052150649d067372ff2904d9602
zuriel0001 <dogbeykwame.kd@gmail.com>
2024-01-31 20:28:56 +0000

    lifo catching updated

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Module for caching Last-In First-Out
+"""
+from collections import OrderedDict
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    Represents an object that allows storing and
+    retrieving items in a dictionary using LIFO
+    removal mechanism when the limit is reached.
+    """
+
+    def __init__(self):
+        """Initializes the cache.
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+            Method that adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last_key)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
+
+    def get(self, key):
+        """
+        Returns an item by key.
+        """
+        return (self.cache_data.get(key, None))
