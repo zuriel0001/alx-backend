70258b49c1c73e2cc4fe3749a653f4bec558fd9b
zuriel0001 <dogbeykwame.kd@gmail.com>
2024-02-09 22:53:35 +0000

    mandatory

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module for Basic Flask app
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """represent default route"""
+    return (render_template("0-index.html",))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+Get locale from request
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """represents Config class"""
+
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+
+    Returns:
+        str: the best match
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """Method for default route
+
+    Returns:
+        html: homepage
+    """
+    return (render_template("3-index.html"))
+
+
+if __name__ == "__main__":
+    app.run()
