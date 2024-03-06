ef742ca0a679a7def475144c26718667eeac27b6
zuriel0001 <dogbeykwame.kd@gmail.com>
2024-03-06 20:49:26 +0000

    Create the Job processor

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -2,22 +2,19 @@
 import { createQueue } from 'kue';
 
 //Create a queue with Kue
-const queue = createQueue({name: 'push_notification_code'});
+const queue = createQueue();
 
-const job = queue.create('push_notification_code', {
-  phoneNumber: '0247627508',
-  message: 'Account registered',
+//Take argument and log to console
+const sendNotification = (phoneNumber, message) => {
+  console.log(
+    `Sending notification to ${phoneNumber},`,
+    'with message:',
+    message,
+  );
+};
+
+//queue process that will listen to new jobs on push_notification_code:
+queue.process('push_notification_code', (job, done) => {
+  sendNotification(job.data.phoneNumber, job.data.message);
+  done();
 });
-
-//queue process that will listen to new jobs on: push_notification_code
-job
-  .on('enqueue', () => {
-    console.log('Notification job created:', job.id);
-  })
-  .on('complete', () => {
-    console.log('Notification job completed');
-  })
-  .on('failed attempt', () => {
-    console.log('Notification job failed');
-  });
-job.save();
